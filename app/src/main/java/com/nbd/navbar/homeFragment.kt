e0472de4df6bc112074ac1e40509a3b27e2155c0
Ignacio Ordaz <narseg.f@gmail.com>
2022-03-21 11:42:41 -0600

    revisar linea 29 y 30 del homefragment por difenrecias de kotlin con el profe

--- app/src/main/java/com/nbd/navbar/homeFragment.kt
+++ app/src/main/java/com/nbd/navbar/homeFragment.kt
@@ -1,19 +1,36 @@
 package com.nbd.navbar
 
+import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.nbd.navbar.databinding.ActivityMainBinding
+import com.nbd.navbar.databinding.FragmentHomeBinding
 
 class homeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // iniciar binding
+        //binding=FragmentHomeBinding.inflate(inflater,container, false)
+        binding=FragmentHomeBinding.inflate(layoutInflater)
+
+        //onclick pasamos a otro fragment
+        val navController=findNavController()
+        binding.buttonHomeToInside.setOnClickListener{
+            //Toast.makeText(context, "preuba", Toast.LENGTH_SHORT).show()
+            //navController.navigate(R.id.action_homeFragment_to_insideHome)
+            //val directions=HomeFragmentDirections.actionHomeFragmentToInsideHome()
+            //navController.navigate(directions)
+        }
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/nbd/navbar/insideHome.kt
+++ app/src/main/java/com/nbd/navbar/insideHome.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nbd.navbar.databinding.FragmentHomeBinding
+import com.nbd.navbar.databinding.FragmentInsideHomeBinding
 
 class insideHome : Fragment() {
 
 
+    private lateinit var binding: FragmentInsideHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inside_home, container, false)
+        binding = FragmentInsideHomeBinding.inflate(layoutInflater)
+
+        val argumentoRecibido =arguments?.getString("argumentoPasado")
+        binding.textView2.setText(argumentoRecibido)
+        return binding.root
     }
 
 }
